7bc20640cc2ed9844ac9851c17fd2deb8d9e5fd0
Abner Chou <zhcjtht@hotmail.com>
2017-07-20 10:53:00 -0400

    fix

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function getRepos(err, res){
 
                 console.log(repo.name, i.toString());
                 fs.mkdirSync(gitDir);
-                git(gitDir, isDebug)(composeUrl(res["data"]), composeUrl(res["data"].parent))
+                git(gitDir, isDebug)(composeUrl(repo), composeUrl(repo.parent))
                     .then(() => delDir(gitDir)) // be good, clean up left overs.
                     .then(() => console.log(gitDir + ' Done.'))
                     .catch((err) => console.log(err));
